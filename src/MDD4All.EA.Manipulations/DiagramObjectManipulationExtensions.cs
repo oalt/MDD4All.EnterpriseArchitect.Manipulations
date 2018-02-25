9779fc3c6d7e1822f4c6061c503ff08b3ef8d5dc
oalt <oa@lacheweg21.de>
2018-02-25 16:35:02 +0100

    Add diagram object manipulations

---
+++ src/MDD4All.EA.Manipulations/DiagramObjectManipulationExtensions.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+#if FACADE
+using EAAPI = MDD4All.EAFacade;
+#else
+using EAAPI = EA;
+#endif
+
+#if EAAPI
+namespace MDD4All.EAFacade.Manipulations
+#else
+namespace MDD4All.EnterpriseArchitect.Manipulations
+#endif
+{
+	public static class DiagramObjectManipulationExtensions
+	{
+		public static Point GetLabelSize(this EAAPI.DiagramObject portDiagramObject)
+		{
+			Point result = new Point();
+
+			string style = portDiagramObject.Style.ToString();
+
+			char[] semicolonSperator = { ';' };
+
+			string[] styleTokens = style.Split(semicolonSperator);
+
+			string labelToken = styleTokens.FirstOrDefault(token => token.StartsWith("LBL="));
+
+			if(!string.IsNullOrEmpty(labelToken))
+			{
+				labelToken = labelToken.Replace("LBL=", "");
+
+				char[] colonSeparator = { ':' };
+
+				string[] labelPropertyTokens = labelToken.Split(colonSeparator);
+
+				string cx = labelPropertyTokens.FirstOrDefault(cxToken => cxToken.StartsWith("CX="))?.Replace("CX=", "");
+
+				string cy = labelPropertyTokens.FirstOrDefault(cxToken => cxToken.StartsWith("CY="))?.Replace("CY=", "");
+
+				int resultX = 0;
+
+				int.TryParse(cx, out resultX);
+
+				int resultY = 0;
+
+				int.TryParse(cy, out resultY);
+
+				result.X = resultX;
+				result.Y = resultY;
+			}
+
+			return result;
+		}
+
+		public static Point GetLabelOffset(this EAAPI.DiagramObject portDiagramObject)
+		{
+			Point result = new Point();
+
+			string style = portDiagramObject.Style.ToString();
+
+			char[] semicolonSperator = { ';' };
+
+			string[] styleTokens = style.Split(semicolonSperator);
+
+			string labelToken = styleTokens.FirstOrDefault(token => token.StartsWith("LBL="));
+
+			if (!string.IsNullOrEmpty(labelToken))
+			{
+				labelToken = labelToken.Replace("LBL=", "");
+
+				char[] colonSeparator = { ':' };
+
+				string[] labelPropertyTokens = labelToken.Split(colonSeparator);
+
+				string cx = labelPropertyTokens.FirstOrDefault(cxToken => cxToken.StartsWith("OX="))?.Replace("OX=", "");
+
+				string cy = labelPropertyTokens.FirstOrDefault(cxToken => cxToken.StartsWith("OY="))?.Replace("OY=", "");
+
+				int resultX = 0;
+
+				int.TryParse(cx, out resultX);
+
+				int resultY = 0;
+
+				int.TryParse(cy, out resultY);
+
+				result.X = resultX;
+				result.Y = resultY;
+			}
+
+			return result;
+		}
+	}
+}
